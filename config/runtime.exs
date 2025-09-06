aa61dc8beacc7e38c1e8e23a1da60d86202e40ea
danielmoessner <github@danielmoessner.de>
2025-09-06 23:03:20 +0200

    updates

--- config/runtime.exs
+++ config/runtime.exs
@@ -24,7 +24,8 @@ if config_env() == :prod do
   config :books, Books.Repo,
     adapter: Ecto.Adapters.SQLite3,
     database: System.get_env("SQLITE_DB_PATH") || "prod.sqlite3",
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+    create_if_not_exists: false
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
